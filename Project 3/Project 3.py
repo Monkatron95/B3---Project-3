db39e377a441b2191d69fa21f4e28112fbfe6513
alexandra15 <mihalcea@uni.coventry.ac.uk>
2015-02-18 00:10:33 +0000

    a few fixes
    - robot movement is smoother
    - quit function works properly

--- Project 3/Project 3.py
+++ Project 3/Project 3.py
@@ -49,24 +49,25 @@ class Robot(Block):
         #for treasure in treasure_list:
         try:
             treasure = treasure_list.sprites()[0]
-            if self.rect.x > treasure.rect.x:
-                self.rect.x -=speed
-            elif self.rect.x < treasure.rect.x:
-                self.rect.x +=speed
+            if abs(self.rect.x - treasure.rect.x)>=speed:
+                if self.rect.x > treasure.rect.x:
+                    self.rect.x -=speed
+                elif self.rect.x < treasure.rect.x:
+                    self.rect.x +=speed
             else:
                 self.rect.x += 0
-            if self.rect.y > treasure.rect.y:
-                self.rect.y -=speed
-            elif self.rect.y < treasure.rect.y:
-                self.rect.y +=speed
+            if abs(self.rect.y - treasure.rect.y)>=speed:
+                if self.rect.y > treasure.rect.y:
+                    self.rect.y -=speed
+                elif self.rect.y < treasure.rect.y:
+                    self.rect.y +=speed
             else:
                 self.rect.y += 0
         except IndexError:
            pass
         
 class Button():
-    def __init__(self, screen, x, y, width, height, text, color, active_color, font,selected):
-        self.screen=screen
+    def __init__(self, x, y, width, height, text, color, active_color, font,selected):
         self.x=x
         self.y=y
         self.width=width
@@ -77,13 +78,16 @@ class Button():
         self.font=font
         self.selected=selected
     #def 
-    def update(self,mouse,treasure_type,screen):
+    def update(self, action, condition):
         if self.x+self.width > mouse[0] > self.x and self.y+self.height > mouse[1] > self.y:
-            pygame.draw.rect(self.screen, self.active_color,(self.x,self.y,self.width,self.height))
+            pygame.draw.rect(screen, self.active_color,(self.x,self.y,self.width,self.height))
             if mouseClick():
-                    generate_treasure(1, treasure_type)
+                if condition is not None:
+                    action(condition)
+                else:
+                    action()
         else:
-            pygame.draw.rect(self.screen, self.color,(self.x,self.y,self.width,self.height))
+            pygame.draw.rect(screen, self.color,(self.x,self.y,self.width,self.height))
         screen.blit(self.text, [self.x+(self.width/3), self.y+(self.height/3)])
 
               
@@ -93,6 +97,7 @@ def main():
     traps_left=0
 
     #set up main screen size and color
+    global screen, mouse
     screen=pygame.display.set_mode((1280,720))
     screen.fill((200,200,200))
     
@@ -100,11 +105,13 @@ def main():
     treasure_map = pygame.image.load("map.png")
 
     #declare treasure types
+    global gold, silver, bronze
     gold=Treasure(300, "treasure_gold.gif")
     silver=Treasure(200, "treasure_silver.gif")
     bronze=Treasure(100, "treasure_bronze.gif")
     
     #declare other stuff
+    global Target, MouseDown, MousePressed, MouseReleased, running
     Target=None # target of Drag/Drop
     MouseDown = False
     MousePressed = False
@@ -114,13 +121,10 @@ def main():
     clock = pygame.time.Clock()
 
     #declare lists
-    global all_sprites_list
+    global all_sprites_list, trap_list, treasure_list, found_list
     all_sprites_list = pygame.sprite.Group() # all sprites
-    global trap_list
     trap_list = pygame.sprite.Group() # all traps
-    global treasure_list
     treasure_list = pygame.sprite.Group() # all treasures
-    global found_list
     found_list=pygame.sprite.Group() # found treasures
     
     # declare robot
@@ -128,37 +132,41 @@ def main():
     all_sprites_list.add(robot)
     
     #declare buttons
-    goldButton= Button(screen, 1050, 30, 180, 70, font.render("Gold", True, (184,134,11)) , (255,215,0), (255,235,0), "arial", False)
-    silverButton= Button(screen, 1050, 130, 180, 70, font.render("Silver", True, (105,105,105)), (160,160,160), (172,172,172), "arial", False)
-    bronzeButton= Button(screen, 1050, 230, 180, 70, font.render("Bronze", True, (184,134,11)), (218,165,32), (238,195,52), "arial", False)
-    
-    # assign a numbe of randomly generated traps
-    #generate_traps(10)
+    goldButton= Button(1050, 30, 180, 70, font.render("Gold", True, (184,134,11)) , (255,215,0), (255,235,0), "arial", False)
+    silverButton= Button(1050, 130, 180, 70, font.render("Silver", True, (105,105,105)), (160,160,160), (172,172,172), "arial", False)
+    bronzeButton= Button(1050, 230, 180, 70, font.render("Bronze", True, (184,134,11)), (218,165,32), (238,195,52), "arial", False)
+    quitButton= Button(1050, 630, 180, 70, font.render("Quit", True, (204, 0, 0)), (153, 0, 0), (102, 0, 0), "arial", False)
+
+
     
 
     while running:
         
-        refreshScreen(screen,treasure_map)
+        #obtain mouse position
         mouse=pygame.mouse.get_pos()
         
-        #print mouse
+        #update screen and screen items
+        refreshScreen(treasure_map)
+        refreshButtons(goldButton, silverButton, bronzeButton, quitButton)
+
+        #select and move treasures
+        selectObjects(treasure_list)
+        
+        #move robot
+        robot.hunt(5)
+
+        #check collision
         traps_hit_list = pygame.sprite.spritecollide(robot, trap_list, True)
         treasure_hit = pygame.sprite.spritecollide(robot, treasure_list, True)
-        
         found_list.add(treasure_hit)
         
-        goldButton.update(mouse, gold,screen)
-        silverButton.update(mouse, silver,screen)
-        bronzeButton.update(mouse, bronze, screen)
 
+        
         #update and display score
         score = font.render(str(robot.score), True, (0,100,0))
         screen.blit(score, [1100, 500])
         
-        Target, MouseDown,MousePressed, MouseReleased = selectObjects(screen, mouse, Target, MouseDown,MousePressed, MouseReleased)
-        
-        #move robot
-        robot.hunt(5)
+
         
         # Check the list of collisions.
         for trap in traps_hit_list:
@@ -189,7 +197,6 @@ def mouseClick():
                 if Event.type == pygame.MOUSEBUTTONDOWN:
                     return True
     return False
-
         
 def generate_traps(number):
     for i in range(number):
@@ -203,11 +210,8 @@ def generate_traps(number):
         all_sprites_list.add(trap)
     return number
 
-
-
-
-def generate_treasure(number, treasureType):
-    for i in range(number):
+def generate_treasure(treasureType):
+    #for i in range(number):
         treasure = Treasure(treasureType.value, treasureType.filename)
 
         # Set a random location for the block
@@ -219,7 +223,6 @@ def generate_treasure(number, treasureType):
         all_sprites_list.add(treasure)
         
 def terminate():
-    print "quit"
     running = False
     pygame.quit()
     sys.exit()
@@ -230,17 +233,24 @@ def releasedMouse():
             return True
     return False
 
-def refreshScreen(screen,treasure_map):
+def refreshScreen(background):
         #clear screen
         screen.fill((200,200,200))
         # clear drawing screen
-        screen.blit(treasure_map, (0,0))
+        screen.blit(background, (0,0))
         #pygame.draw.rect(screen, (235, 235, 235) , [0, 0, 1000, 720])
         # draw objects
         all_sprites_list.draw(screen)
-
+        
+def refreshButtons(goldButton, silverButton, bronzeButton, quitButton):
+        #update buttons
+        goldButton.update(generate_treasure, gold)
+        silverButton.update(generate_treasure, silver)
+        bronzeButton.update(generate_treasure, bronze)
+        quitButton.update(terminate, None)
             
-def selectObjects(screen, mouse, Target, MouseDown,MousePressed, MouseReleased):
+def selectObjects(object_list):
+            global Target, MouseDown, MousePressed, MouseReleased, running
             for Event in pygame.event.get():
                 if Event.type == pygame.MOUSEBUTTONDOWN:
                     MousePressed=True 
@@ -253,7 +263,7 @@ def selectObjects(screen, mouse, Target, MouseDown,MousePressed, MouseReleased):
 
             if MousePressed==True:
                 if Target is None:
-                    for treasure in treasure_list: # search all items
+                    for treasure in object_list: # search all items
                             if treasure.rect.collidepoint(mouse):
                                 Target=treasure # "pick up" item
             if MouseDown and Target is not None: # if dragging
@@ -264,15 +274,14 @@ def selectObjects(screen, mouse, Target, MouseDown,MousePressed, MouseReleased):
                 Target=None # drop item
             return Target, MouseDown,MousePressed, MouseReleased
 
-
- 
 def checkForQuit():
-    for event in pygame.event.get(QUIT): # get all the QUIT events
-        terminate() # terminate if any QUIT events are present
-    for event in pygame.event.get(KEYUP): # get all the KEYUP events
-        if event.key == K_ESCAPE:
-            terminate() # terminate if the KEYUP event was for the Esc key
-        pygame.event.post(event) # put the other KEYUP event objects back
+    Event = pygame.event.wait ()
+    if Event.type == QUIT:
+        terminate()
+    if Event.type == KEYDOWN:
+        print Event.key
+        if Event.key == K_ESCAPE:
+            terminate()
 
 if __name__ == '__main__':
     main() # Execute main function
